a24158ef87ef60f12fea457367aa7424a2d55bee
Sergio Velandia <sergioorlandovq@gmail.com>
2025-10-02 21:06:15 -0500

    Consulta de saldo

--- CajeroAutomático/Cajero.cs
+++ CajeroAutomático/Cajero.cs
@@ -76,6 +76,10 @@ namespace CajeroAutomático
             }
             return false;
         }
+        public void ConsultarSaldo()
+        {
+            Console.WriteLine($"Su saldo actual es:  ${this.Saldo:N2}");
+        }
     }
 }
 
